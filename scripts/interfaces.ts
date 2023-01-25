a16573fb40731654222a18f8b3dfaceb0a5df800
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-01-25 13:04:36 +1100

    Collect component files

--- scripts/interfaces.ts
+++ scripts/interfaces.ts
@@ -8,7 +8,8 @@ import { mkdir, readFile, writeFile } from 'node:fs/promises'
 import { getInfo } from './shared/general.js'
 import { Class } from './interfaces/types.js'
 import { parse as parseIDL } from './parser/index.js'
-import { existsSync } from 'node:fs'
+import { existsSync, readFileSync } from 'node:fs'
+import './interfaces/modules.js'
 
 const CLASSES_IDENTIFIER = Sk.ffi.remapToPy('Classes')
 const INVALID_INTERFACES: string[] = [
@@ -38,9 +39,9 @@ const interfaceFiles = interfaceFileLocations
       !INVALID_INTERFACES.includes(name) && !FROM_INVALID_PATH(path)
   )
 
-const failedFiles = []
+const failedFiles: any[] = []
 const classes: Class[] = []
-const typedClasses: Class[] = []
+const interfacedClasses: Class[] = []
 
 Sk.configure({ output: console.info })
 for (const componentFilePath of componentFiles) {
@@ -48,14 +49,72 @@ for (const componentFilePath of componentFiles) {
     const fileContents = await readFile(componentFilePath, {
       encoding: 'utf-8',
     })
-    const module = Sk.importMainWithBody('<stdin>', false, fileContents, false)
+
+    const module = Sk.importMainWithBody(
+      '<stdin>',
+      false,
+      `
+import buildconfig    
+
+def defined(_any):
+      return True
+
+# From: https://searchfox.org/mozilla-central/source/xpcom/components/gen_static_components.py#58
+class ProcessSelector:
+    ANY_PROCESS = 0x0
+    MAIN_PROCESS_ONLY = 0x1
+    CONTENT_PROCESS_ONLY = 0x2
+    ALLOW_IN_GPU_PROCESS = 0x4
+    ALLOW_IN_VR_PROCESS = 0x8
+    ALLOW_IN_SOCKET_PROCESS = 0x10
+    ALLOW_IN_RDD_PROCESS = 0x20
+    ALLOW_IN_UTILITY_PROCESS = 0x30
+    ALLOW_IN_GPU_AND_MAIN_PROCESS = ALLOW_IN_GPU_PROCESS | MAIN_PROCESS_ONLY
+    ALLOW_IN_GPU_AND_SOCKET_PROCESS = ALLOW_IN_GPU_PROCESS | ALLOW_IN_SOCKET_PROCESS
+    ALLOW_IN_GPU_AND_VR_PROCESS = ALLOW_IN_GPU_PROCESS | ALLOW_IN_VR_PROCESS
+    ALLOW_IN_GPU_VR_AND_SOCKET_PROCESS = (
+        ALLOW_IN_GPU_PROCESS | ALLOW_IN_VR_PROCESS | ALLOW_IN_SOCKET_PROCESS
+    )
+    ALLOW_IN_RDD_AND_SOCKET_PROCESS = ALLOW_IN_RDD_PROCESS | ALLOW_IN_SOCKET_PROCESS
+    ALLOW_IN_GPU_RDD_AND_SOCKET_PROCESS = (
+        ALLOW_IN_GPU_PROCESS | ALLOW_IN_RDD_PROCESS | ALLOW_IN_SOCKET_PROCESS
+    )
+    ALLOW_IN_GPU_RDD_SOCKET_AND_UTILITY_PROCESS = (
+        ALLOW_IN_GPU_PROCESS
+        | ALLOW_IN_RDD_PROCESS
+        | ALLOW_IN_SOCKET_PROCESS
+        | ALLOW_IN_UTILITY_PROCESS
+    )
+    ALLOW_IN_GPU_RDD_VR_AND_SOCKET_PROCESS = (
+        ALLOW_IN_GPU_PROCESS
+        | ALLOW_IN_RDD_PROCESS
+        | ALLOW_IN_VR_PROCESS
+        | ALLOW_IN_SOCKET_PROCESS
+    )
+    ALLOW_IN_GPU_RDD_VR_SOCKET_AND_UTILITY_PROCESS = (
+        ALLOW_IN_GPU_PROCESS
+        | ALLOW_IN_RDD_PROCESS
+        | ALLOW_IN_VR_PROCESS
+        | ALLOW_IN_SOCKET_PROCESS
+        | ALLOW_IN_UTILITY_PROCESS
+    )
+
+class BackgroundTasksSelector:
+    NO_TASKS = 0x0
+    ALL_TASKS = 0xFFFF
+
+
+${fileContents}`,
+      false
+    )
+
     const moduleClasses = Sk.ffi.remapToJs(
       module.tp$getattr(CLASSES_IDENTIFIER)
     )
 
     for (const moduleClass of moduleClasses) {
-      if (moduleClass.type) {
-        typedClasses.push({
+      if (moduleClass.interfaces) {
+        interfacedClasses.push({
           componentFile: componentFilePath,
           ...moduleClass,
         })
@@ -68,14 +127,15 @@ for (const componentFilePath of componentFiles) {
       })
     }
   } catch (e) {
-    failedFiles.push(componentFilePath)
+    failedFiles.push({ path: componentFilePath, e })
   }
 }
+
+console.log('Components done')
 
 let failedInterfaceFiles = []
 
 for (const interfaceFile of interfaceFiles) {
-  console.log(interfaceFile.path)
   const fileContents = await readFile(interfaceFile.path, 'utf-8')
   const { errs, ast } = parseIDL(fileContents)
 
@@ -95,10 +155,12 @@ for (const interfaceFile of interfaceFiles) {
   )
 }
 
+console.log('IDL done')
+
 if (!existsSync(join(outFolder, '_')))
   await mkdir(join(outFolder, '_'), { recursive: true })
 
 await writeFile(
   join(outFolder, '_', 'classes.json'),
-  JSON.stringify({ failedFiles, classes, typedClasses }, null, 2)
+  JSON.stringify({ failedFiles, classes, interfacedClasses }, null, 2)
 )
